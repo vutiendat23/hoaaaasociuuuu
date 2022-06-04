3eac2b7ec49b5afcca616afb7e481e5b97369a41
vutiendat23 <106805828+vutiendat23@users.noreply.github.com>
2022-06-04 11:22:44 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Thế giới của Đạt",
+    introTitle: 'Nguyễn Quỳnh Hoa',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Nếu đây là giấc mơ thì hãy để anh chìm thật sâu 🥰',
+    desc: 'Anh không muốn thức dậy để lỡ vụt mất em nữa đâu ',
+    btnYes: 'Em yêu Anh <33',
+    btnNo: 'Ảo tưởng ít thooii :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Anh là điều tuyệt vời nhất mà em từng biết-em yêu anh nhiều lắmm <33333333',
+    mess: 'Anh biết mà 🥰. yêu em nhấttt 😘😘',
+    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    btnAccept: 'vâng ạ <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100013126022097'  }
